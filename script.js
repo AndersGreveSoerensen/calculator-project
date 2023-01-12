5507ddf063d61fdf08b6b08ae20fd296f87bf5c5
Anders Greve Sørensen <a.grevesoerensen@gmail.com>
2023-01-12 17:35:14 +0100

    Setup html boilerplate and basic math logic.

---
+++ script.js
@@ -0,0 +1,21 @@
+function add(a, b) {
+    return a + b
+}
+
+function subtract(a, b) {
+    return a - b
+}
+
+function multiply(a, b) {
+    return a * b
+}
+
+function divide(a, b) {
+    return a / b
+}
+
+function operate (a, b, operator) {
+    return operator(a, b)
+}
+
+console.log(operate(3, 5, divide))
